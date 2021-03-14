c0794ce185b2281f6b4ddfda1dd60c9a7e8b6ef0
sshHorchata <73266666+sshHorchata@users.noreply.github.com>
2021-03-13 18:29:13 -0700

    Add files via upload

---
+++ AOX.py
@@ -0,0 +1,14 @@
+name = "VrongoCubed"
+print("  \o\       /o/  oooooo\     oooo    oooo  o   ooooo    oooooo   ")
+print("   \oo\   /oo/   oo   oo    o    o   oo oo o  o        o      o  ")
+print("    \oo\ /oo/    ooooo\     o    o   oo  ooo  ooooooo  o      o  ")
+print("     \oooo/      oo/ \oo\   o    o   oo   oo  o     o  o      o  ")
+print("      \oo/       o    \o\    oooo    o     o  oooooo    oooooo   ")
+print("   _____    _____    _____    _____    _____    _____    _____    ")
+print("  /\    \  /\    \  /\    \  /\    \  /\    \  /\    \  /\    \   ")
+print(" /  \____\/  \____\/  \____\/  \____\/  \____\/  \____\/  \____\  " )
+print(" \  /    /\  /    /\  /    /\  /    /\  /    /\  /    /\  /    /  " )
+print("  \/____/  \/____/  \/____/  \/____/  \/____/  \/____/  \/____/   ")
+
+
+
